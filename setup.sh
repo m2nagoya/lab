0a619278d422849736eb2ab69e48820d220b4fdc
m2nagoya <sakura42t@yahoo.co.jp>
2020-08-08 08:56:31 +0900

    Modified

--- setup.sh
+++ setup.sh
@@ -12,66 +12,6 @@ option1[5]="CUDA"
 option2[0]="10.2"
 option2[1]="11.0"
 
-COMMAND () {
-  for n in ${!option1[@]} ; do
-    if [[ ${choices1[$n]} ]]; then
-      SETUP()
-      case "$n" in
-        "0" ) OPENSSH       ;;
-        "1" ) GITHUB        ;;
-        "2" ) ANACONDA      ;;
-        "3" ) PIP3          ;;
-        "4" ) DRIVER        ;;
-        "5" ) MAIN_SELECT 2 ;;
-      esac
-    fi
-  done
-  for n in ${!option2[@]} ; do
-    if [[ ${choices2[$n]} ]]; then
-      SETUP()
-      case "$n" in
-        "0" ) CUDA ${option2[$n]} ;;
-        "1" ) CUDA ${option2[$n]} ;;
-      esac
-    fi
-  done
-}
-
-function MAIN_SELECT {
-  current_line=0
-  case "$1" in
-    "1"  ) OPTION=("${option1[@]}")  ;;
-    "2"  ) OPTION=("${option2[@]}")  ;;
-  esac
-  while clear && MAIN_MENU $1 && IFS= read -r -n1 -s SELECTION && [[ -n "$SELECTION" ]]; do
-    [[ $SELECTION == $'\x1b' ]] && read -r -n2 -s rest && SELECTION+="$rest"
-    clear ; case $SELECTION in
-    $'\x1b\x5b\x41' ) [[ $current_line -ne 0 ]]                      && current_line=$(( current_line - 1 )) || current_line=$(( ${#OPTION[@]}-1 )) ;;
-    $'\x1b\x5b\x42' ) [[ $current_line -ne $(( ${#OPTION[@]}-1 )) ]] && current_line=$(( current_line + 1 )) || current_line=0                      ;;
-    $'\x20'         )
-    case $1 in
-      "1"  ) [[ "${choices1[current_line]}" == "+"  ]] && choices1[current_line]=""  || choices1[current_line]="+"  ;;
-      "2"  ) [[ "${choices2[current_line]}" == "+"  ]] && choices2[current_line]=""  || choices2[current_line]="+"  ;;
-    esac
-  esac
-done
-}
-
-function MAIN_MENU {
-  case "$1" in
-    "1"  ) echo -e "Which do you install ?\n" ;;
-    "2"  ) echo -e "Which do you select ?\n" ;;
-  esac
-  echo "移動:[↑]or[↓], 選択:[SPACE], 決定:[ENTER]"
-  for n in ${!OPTION[@]}; do
-    [ $n -eq $current_line ] && echo -n ">" || echo -n " "
-    case "$1" in
-      "1"  ) echo -e "[${choices1[$n]:- }]: ${option1[$n]}"   ;;
-      "2"  ) echo -e "[${choices2[$n]:- }]: ${option2[$n]}"   ;;
-    esac
-  done
-}
-
 function SETUP {
   sudo apt -y upgrade
   sudo apt -y autoremove
@@ -131,6 +71,66 @@ function CUDA {
     sudo dpkg -i cuDNN8.0.2/libcudnn8-dev_8.0.2.39-1+cuda11.0_amd64.deb
     sudo dpkg -i cuDNN8.0.2/libcudnn8-doc_8.0.2.39-1+cuda11.0_amd64.deb
   fi
+}
+
+function COMMAND () {
+  for n in ${!option1[@]} ; do
+    if [[ ${choices1[$n]} ]]; then
+      SETUP()
+      case "$n" in
+        "0" ) OPENSSH       ;;
+        "1" ) GITHUB        ;;
+        "2" ) ANACONDA      ;;
+        "3" ) PIP3          ;;
+        "4" ) DRIVER        ;;
+        "5" ) MAIN_SELECT 2 ;;
+      esac
+    fi
+  done
+  for n in ${!option2[@]} ; do
+    if [[ ${choices2[$n]} ]]; then
+      SETUP()
+      case "$n" in
+        "0" ) CUDA ${option2[$n]} ;;
+        "1" ) CUDA ${option2[$n]} ;;
+      esac
+    fi
+  done
+}
+
+function MAIN_SELECT {
+  current_line=0
+  case "$1" in
+    "1"  ) OPTION=("${option1[@]}")  ;;
+    "2"  ) OPTION=("${option2[@]}")  ;;
+  esac
+  while clear && MAIN_MENU $1 && IFS= read -r -n1 -s SELECTION && [[ -n "$SELECTION" ]]; do
+    [[ $SELECTION == $'\x1b' ]] && read -r -n2 -s rest && SELECTION+="$rest"
+    clear ; case $SELECTION in
+    $'\x1b\x5b\x41' ) [[ $current_line -ne 0 ]]                      && current_line=$(( current_line - 1 )) || current_line=$(( ${#OPTION[@]}-1 )) ;;
+    $'\x1b\x5b\x42' ) [[ $current_line -ne $(( ${#OPTION[@]}-1 )) ]] && current_line=$(( current_line + 1 )) || current_line=0                      ;;
+    $'\x20'         )
+    case $1 in
+      "1"  ) [[ "${choices1[current_line]}" == "+"  ]] && choices1[current_line]=""  || choices1[current_line]="+"  ;;
+      "2"  ) [[ "${choices2[current_line]}" == "+"  ]] && choices2[current_line]=""  || choices2[current_line]="+"  ;;
+    esac
+  esac
+done
+}
+
+function MAIN_MENU {
+  case "$1" in
+    "1"  ) echo -e "Which do you install ?\n" ;;
+    "2"  ) echo -e "Which do you select ?\n" ;;
+  esac
+  echo "移動:[↑]or[↓], 選択:[SPACE], 決定:[ENTER]"
+  for n in ${!OPTION[@]}; do
+    [ $n -eq $current_line ] && echo -n ">" || echo -n " "
+    case "$1" in
+      "1"  ) echo -e "[${choices1[$n]:- }]: ${option1[$n]}"   ;;
+      "2"  ) echo -e "[${choices2[$n]:- }]: ${option2[$n]}"   ;;
+    esac
+  done
 }
 
 MAIN_SELECT 1 ; COMMAND
